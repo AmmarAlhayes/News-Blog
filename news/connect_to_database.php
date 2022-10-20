31a866995c05d06a39739174a0d7036e3ba31e7c
AmmarAlhayes <80069876+AmmarAlhayes@users.noreply.github.com>
2022-10-20 22:31:43 +0200

    Update connect_to_database.php

--- news/connect_to_database.php
+++ news/connect_to_database.php
@@ -2,12 +2,9 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	//$db="testdb";
-    //$db="newsdb";
-    $db="news_db";
+    	$db="news_db";
 	/* Create connection */
 	$conn = mysqli_connect($servername, $username, $password, $db);
 
-	//echo "hallo";
 
 ?>
